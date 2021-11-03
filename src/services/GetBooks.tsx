c99a6b970ce83536978f0dd0912c9c2f9c3c3bfa
Jenny <jenny@elis-MacBook-Air.local>
2021-11-03 18:35:23 -0400

    API stuff displaying

--- src/services/GetBooks.tsx
+++ src/services/GetBooks.tsx
@@ -1,11 +1,18 @@
 import axios from "axios";
-import { DocsEntity } from "../models/BookDetailsInterface";
+import {
+  BookDetailInterface,
+  DocsEntity,
+} from "../models/BookDetailsInterface";
+import SearchParams from "../models/SearchParams";
 
-export default function getBooks(searchQuery: string): Promise<DocsEntity> {
+//searchQuery is being used here bc whatever is being put into getBooks goes into the end of the url
+export default function getBooks(
+  searchQuery: string
+): Promise<BookDetailInterface> {
   return axios
     .get("http://openlibrary.org/search.json?", {
       params: {
-        q: "the+lord+of+the+rings",
+        q: searchQuery, //this the parameter of getBooks() - adds onto the url so that it can retrieve the specific search
       },
     })
     .then((response) => {
